019b12aa950987c223639c696b2314e1a9d6550e
A. van Rossum <36618231+ator1811@users.noreply.github.com>
2026-02-13 17:48:10 +0100

    make functions extern

--- shared-module/simplefoc/PIDController.h
+++ shared-module/simplefoc/PIDController.h
@@ -16,17 +16,14 @@ typedef struct {
     uint64_t timestamp_prev;
 } simplefoc_pidcontroller_obj_t;
 
-void common_hal_simplefoc_pidcontroller_construct(simplefoc_pidcontroller_obj_t *self,
-                                                    float P, float I, float D,
-                                                    float ramp, float limit);
-float common_hal_simplefoc_pidcontroller_calculate(simplefoc_pidcontroller_obj_t *self,
-                                                     float setpoint, float measured, float dt);
-void common_hal_simplefoc_pidcontroller_reset(simplefoc_pidcontroller_obj_t *self);
-float common_hal_simplefoc_pidcontroller_get_P(simplefoc_pidcontroller_obj_t *self);
-void common_hal_simplefoc_pidcontroller_set_P(simplefoc_pidcontroller_obj_t *self, float P);
-float common_hal_simplefoc_pidcontroller_get_I(simplefoc_pidcontroller_obj_t *self);
-void common_hal_simplefoc_pidcontroller_set_I(simplefoc_pidcontroller_obj_t *self, float I);
-float common_hal_simplefoc_pidcontroller_get_D(simplefoc_pidcontroller_obj_t *self);
-void common_hal_simplefoc_pidcontroller_set_D(simplefoc_pidcontroller_obj_t *self, float D);
+extern void common_hal_simplefoc_pidcontroller_construct(simplefoc_pidcontroller_obj_t *self, float P, float I, float D, float ramp, float limit);
+extern float common_hal_simplefoc_pidcontroller_calculate(simplefoc_pidcontroller_obj_t *self, float setpoint, float measured, float dt);
+extern void common_hal_simplefoc_pidcontroller_reset(simplefoc_pidcontroller_obj_t *self);
+extern float common_hal_simplefoc_pidcontroller_get_P(simplefoc_pidcontroller_obj_t *self);
+extern void common_hal_simplefoc_pidcontroller_set_P(simplefoc_pidcontroller_obj_t *self, float P);
+extern float common_hal_simplefoc_pidcontroller_get_I(simplefoc_pidcontroller_obj_t *self);
+extern void common_hal_simplefoc_pidcontroller_set_I(simplefoc_pidcontroller_obj_t *self, float I);
+extern float common_hal_simplefoc_pidcontroller_get_D(simplefoc_pidcontroller_obj_t *self);
+extern void common_hal_simplefoc_pidcontroller_set_D(simplefoc_pidcontroller_obj_t *self, float D);
 
 #endif // MICROPY_INCLUDED_SHARED_MODULE_SIMPLEFOC_PIDCONTROLLER_H
--- shared-module/simplefoc/SpeedCalculator.h
+++ shared-module/simplefoc/SpeedCalculator.h
@@ -12,6 +12,10 @@ typedef struct {
     bool initialized;
 } simplefoc_speedcalculator_obj_t;
 
+extern void common_hal_simplefoc_speedcalculator_construct(simplefoc_speedcalculator_obj_t *self, int ppr, float min_dt);
+extern float common_hal_simplefoc_speedcalculator_calculate(simplefoc_speedcalculator_obj_t *self, int position, float dt);
+extern void common_hal_simplefoc_speedcalculator_reset(simplefoc_speedcalculator_obj_t *self);
+
 void common_hal_simplefoc_speedcalculator_construct(simplefoc_speedcalculator_obj_t *self,
                                                       int ppr, float min_dt);
 float common_hal_simplefoc_speedcalculator_calculate(simplefoc_speedcalculator_obj_t *self,
