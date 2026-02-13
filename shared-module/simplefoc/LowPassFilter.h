a6eced21fe76a94b79c78774189de491aa5084ce
A. van Rossum <36618231+ator1811@users.noreply.github.com>
2026-02-13 17:48:10 +0100

    make functions extern

--- shared-module/simplefoc/LowPassFilter.h
+++ shared-module/simplefoc/LowPassFilter.h
@@ -9,10 +9,10 @@ typedef struct {
     float y_prev;       // Previous output
 } simplefoc_lowpassfilter_obj_t;
 
-void common_hal_simplefoc_lowpassfilter_construct(simplefoc_lowpassfilter_obj_t *self, float Tf);
-float common_hal_simplefoc_lowpassfilter_filter(simplefoc_lowpassfilter_obj_t *self, float value, float dt);
-void common_hal_simplefoc_lowpassfilter_reset(simplefoc_lowpassfilter_obj_t *self);
-float common_hal_simplefoc_lowpassfilter_get_Tf(simplefoc_lowpassfilter_obj_t *self);
-void common_hal_simplefoc_lowpassfilter_set_Tf(simplefoc_lowpassfilter_obj_t *self, float Tf);
+extern void common_hal_simplefoc_lowpassfilter_construct(simplefoc_lowpassfilter_obj_t *self, float Tf);
+extern float common_hal_simplefoc_lowpassfilter_filter(simplefoc_lowpassfilter_obj_t *self, float value, float dt);
+extern void common_hal_simplefoc_lowpassfilter_reset(simplefoc_lowpassfilter_obj_t *self);
+extern float common_hal_simplefoc_lowpassfilter_get_Tf(simplefoc_lowpassfilter_obj_t *self);
+extern void common_hal_simplefoc_lowpassfilter_set_Tf(simplefoc_lowpassfilter_obj_t *self, float Tf);
 
 #endif // MICROPY_INCLUDED_SHARED_MODULE_SIMPLEFOC_LOWPASSFILTER_H
