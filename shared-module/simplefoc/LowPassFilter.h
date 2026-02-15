5b5014a4ae27dd9f6cfe824df8ab044c2e17554a
A. van Rossum <36618231+ator1811@users.noreply.github.com>
2026-02-15 08:46:28 +0100

    new version of LowPassFilter

--- shared-module/simplefoc/LowPassFilter.h
+++ shared-module/simplefoc/LowPassFilter.h
@@ -4,7 +4,7 @@
 
 // Function declarations for implementation
 extern void common_hal_simplefoc_lowpassfilter_construct(simplefoc_lowpassfilter_obj_t *self, float Tf);
-extern float common_hal_simplefoc_lowpassfilter_filter(simplefoc_lowpassfilter_obj_t *self, float x, float dt);
+extern float common_hal_simplefoc_lowpassfilter_filter(simplefoc_lowpassfilter_obj_t *self, float x);
 extern void common_hal_simplefoc_lowpassfilter_reset(simplefoc_lowpassfilter_obj_t *self);
 extern float common_hal_simplefoc_lowpassfilter_get_Tf(simplefoc_lowpassfilter_obj_t *self);
 extern void common_hal_simplefoc_lowpassfilter_set_Tf(simplefoc_lowpassfilter_obj_t *self, float Tf);
